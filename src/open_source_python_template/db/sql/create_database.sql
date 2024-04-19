49950de19cea9c84680120d348e316c1b81b4137
Fan Yang <993169208@qq.com>
2024-04-19 01:18:05 -0400

    add sql file to create database

---
+++ src/open_source_python_template/db/sql/create_database.sql
@@ -0,0 +1,9 @@
+CREATE TABLE comments_with_assigned_tasks (
+    id SERIAL PRIMARY KEY,
+    assignee VARCHAR(255) NOT NULL,
+    assigner VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    quoted_file_content TEXT,
+    created_time TIMESTAMPTZ NOT NULL,
+    source_document_url TEXT NOT NULL
+);
