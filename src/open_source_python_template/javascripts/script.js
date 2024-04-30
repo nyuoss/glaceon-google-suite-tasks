9149d27293427ea42148a6b4568683c9d9a9798a
zwinniez <wz1509@nyu.edu>
2024-04-29 23:31:31 -0400

    feat: working search bar for tasklist

--- src/open_source_python_template/javascripts/script.js
+++ src/open_source_python_template/javascripts/script.js
@@ -5,3 +5,18 @@ function toggleDropdown(detailId) {
         element.classList.toggle('show');
     }
 }
+
+function search_task(id) {
+    let input = document.getElementById(id).value.toLowerCase();
+  console.log(input);
+    let tasklists = document.getElementsByClassName("category");
+    for (i=0; i<tasklists.length; i++){
+        if (tasklists[i].innerHTML.toLowerCase().includes(input)) {
+            console.log(tasklists[i]);
+            tasklists[i].style.display = "list-item";
+        } else {
+            console.log(tasklists[i]);
+            tasklists[i].style.display = "none";
+        }
+    }
+}
