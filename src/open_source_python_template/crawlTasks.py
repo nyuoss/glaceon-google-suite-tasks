f9926a1e3c74a0b0279c1a223450fc1d82bb09a2
Kaining Mao <chrissmao99@gmail.com>
2024-05-08 18:21:27 -0400

    Remove request
    Remove request

--- src/open_source_python_template/crawlTasks.py
+++ src/open_source_python_template/crawlTasks.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 import os.path
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
