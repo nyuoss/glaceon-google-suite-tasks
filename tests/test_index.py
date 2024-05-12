46ef9fa8cce4ed7d8809a2f80373115d7de79ad2
Yanglin-Tao <81639920+Yanglin-Tao@users.noreply.github.com>
2024-05-12 15:04:34 -0400

    Add frontend and backend tests

---
+++ tests/test_index.py
@@ -0,0 +1,64 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+@pytest.fixture(scope="class")
+def browser():
+    # Setup Selenium
+    service = Service(executable_path='/opt/homebrew/bin/chromedriver')
+    options = Options()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.maximize_window()  
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
+
+class TestIndex:
+    # Setup browser 
+    @pytest.fixture(autouse=True)
+    def setup(self, browser):
+        self.browser = browser
+        self.browser.get("http://127.0.0.1:5000") 
+
+    # Check if page title is correctly displayed
+    def test_page_title(self):
+        expected_title = "Tasks Dashboard"
+        assert self.browser.title == expected_title, f"Expected page title to be '{expected_title}' but got '{self.browser.title}'"
+
+    # Check if username is correctly displayed
+    def test_username_displayed(self):
+        username_span = self.browser.find_element(By.CSS_SELECTOR, "span.username-placeholder")
+        expected_username = "username" 
+        assert username_span.text == expected_username
+
+    # Check if search bar is correctly displayed
+    def test_search_bar_displayed(self):
+        search_bar = self.browser.find_element(By.ID, "search-tasklists")
+        assert search_bar.is_displayed()
+
+    # Check if task lists are correctly displayed
+    def test_task_categories_displayed(self):
+        task_categories = self.browser.find_element(By.ID, 'task-categories')
+        assert task_categories.is_displayed()
+
+    # Check if task itms are correctly displayed
+    def test_task_item_displayed(self):
+        task_item = self.browser.find_element(By.CSS_SELECTOR, ".task-item")
+        assert task_item.is_displayed()
+
+    # Check if dropdown is correctly displayed
+    def test_dropdown_displayed(self):
+        dropdown_button = self.browser.find_element(By.CSS_SELECTOR, ".dropdown-toggle")
+        assert dropdown_button.is_displayed(), "Dropdown button is not displayed"
+
+    # Check if checkbox correctly displayed and unselected
+    def test_task_checkbox_displayed(self):
+        checkbox = self.browser.find_element(By.ID, "task1")
+        assert checkbox.is_displayed()
+        assert not checkbox.is_selected()
