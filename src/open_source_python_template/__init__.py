c00b910f1faa4ceac10ae0cfd9a1a377679fd824
zwinniez <wz1509@nyu.edu>
2024-05-12 13:47:27 -0400

    fix: fixed formatting

--- src/open_source_python_template/__init__.py
+++ src/open_source_python_template/__init__.py
@@ -11,5 +11,6 @@ print(tasks)
 def index():
     return render_template("index.html", tasks=tasks)
 
+
 if __name__ == "__main__":
     app.run(debug=True)
--- src/open_source_python_template/crawlTasks.py
+++ src/open_source_python_template/crawlTasks.py
@@ -18,9 +18,7 @@ def get_tasks():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                "credential.json", SCOPES
-            )
+            flow = InstalledAppFlow.from_client_secrets_file("credential.json", SCOPES)
             creds = flow.run_local_server(port=0)
             with open("token.json", "w") as token:
                 token.write(creds.to_json())
