6daecedcf4e203e7f41ddfa628dab75130297c5b
zwinniez <wz1509@nyu.edu>
2024-05-12 13:42:57 -0400

    feat: view tasks retrieved from task crawler on webpage

--- src/open_source_python_template/__init__.py
+++ src/open_source_python_template/__init__.py
@@ -9,8 +9,7 @@ print(tasks)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
-
+    return render_template("index.html", tasks=tasks)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- src/open_source_python_template/crawlTasks.py
+++ src/open_source_python_template/crawlTasks.py
@@ -19,7 +19,7 @@ def get_tasks():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                "src/open_source_python_template/credential.json", SCOPES
+                "credential.json", SCOPES
             )
             creds = flow.run_local_server(port=0)
             with open("token.json", "w") as token:
