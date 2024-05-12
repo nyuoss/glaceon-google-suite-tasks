1b7f1f43ca50407913f7002f5afa43dcd7bffb59
Kaining Mao <chrissmao99@gmail.com>
2024-05-11 22:48:58 -0400

    Update crawlTasks.py
    The task information is returned

--- src/open_source_python_template/__init__.py
+++ src/open_source_python_template/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
+import crawlTasks
 
 app = Flask(__name__)
 
+tasks = crawlTasks.get_tasks()
+print(tasks)
 
 @app.route("/")
 def index():
     return render_template("index.html")
-
-
+            
 if __name__ == "__main__":
     app.run(debug=True)
